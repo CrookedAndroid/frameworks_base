8176dfbf8f97c48028f14c7e810874c22f2db8cf
jhonboy121 <alfredmathew05@gmail.com>
2021-12-07 17:04:23 +0800

    base: allow hiding navbar [1/2]
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Change-Id: I0cec7676ced92ba0bfca81e5ff85a3fd189fb5b2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5613,6 +5613,13 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
 
         /**
+         * Whether to hide navbar pill and keyboard space.
+         * Default 0.
+         * @hide
+         */
+        public static final String FULLSCREEN_GESTURES = "fullscreen_gestures";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
