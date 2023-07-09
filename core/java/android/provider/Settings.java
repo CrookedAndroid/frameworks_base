76cfed5cc1ba10e3ebda608ac0909e47e9a61aed
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-10 02:17:41 +0800

    SystemUI: QS battery customizations [1/2]
    * disable qs estimates as well
    
    Change-Id: Iddd308f82fd991ba1a55bfc2b497f3c082598545
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5644,6 +5644,16 @@ public final class Settings {
         public static final String SEEK_BAR_BATTERY_CHARGE_LEVEL_SOUND = "battery_level_charge_seek_bar";
 
         /**
+         * @hide
+         */
+        public static final String QS_BATTERY_STYLE = "qs_battery_style";
+
+        /**
+         * @hide
+         */
+        public static final String QS_SHOW_BATTERY_PERCENT = "qs_show_battery_percent";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
