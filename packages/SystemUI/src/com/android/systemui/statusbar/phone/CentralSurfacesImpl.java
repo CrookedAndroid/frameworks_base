9e5f3e97a6f4a2168f3527edd012ca852da72b44
Dabug123 <clarkbrunswick55@gmail.com>
2023-07-24 23:22:12 +0000

    Revert "SystemUI: Add auto brightness button to QS brightness slider"
    This reverts commit 8e926d0ed1e435aa997cbbb4fb7587578fbb3759.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1352,7 +1352,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
                             .withDefault(this::createDefaultQSFragment)
                             .build());
             mBrightnessMirrorController = new BrightnessMirrorController(
-                    mContext,
                     mNotificationShadeWindowView,
                     mNotificationPanelViewController,
                     mNotificationShadeDepthControllerLazy.get(),
